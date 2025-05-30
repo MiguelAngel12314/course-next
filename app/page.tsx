6f7a9cdc33430e1a68821f7f24f899a519a90d3b
Miguel Gonzalez <miguel12314@hotmail.es>
2025-05-29 20:04:31 -0600

    update text home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <span className="text-5xl bg-red-500">Hola padrino!!!</span>
+      <span className="text-5xl bg-red-500">Hola padrino!!! Soy chanti chanti, cocooooooo</span>
       <Link href={'/contact'}>Contact</Link>
     </main>
   );

2e3e0c4379e5ff874c593b70faa7ac1c6508f225
Miguel Gonzalez <miguel12314@hotmail.es>
2025-05-29 20:20:38 -0600

    add chanti chanti image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
+import chantiImage from '@/app/assests/img/20250524_074524.jpg'
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
       <span className="text-5xl bg-red-500">Hola padrino!!! Soy chanti chanti, cocooooooo</span>
+      <Image src={chantiImage} width={500} height={500} alt='Imagen del reicito'/>
       <Link href={'/contact'}>Contact</Link>
     </main>
   );
